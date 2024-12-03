914dd484486587d957f009419463b937501b3b5e
ErickfuentesIT <erickfuentes.it@gmail.com>
2024-12-03 09:53:34 -0600

    Creación proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module sv.edu.udb.desafio2_ped {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens sv.edu.udb.desafio2_ped to javafx.fxml;
+    exports sv.edu.udb.desafio2_ped;
+}
---
+++ src/main/java/sv/edu/udb/desafio2_ped/HelloController.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.desafio2_ped;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
